6e17b84d7b22f385eee89fb299c659134e5d7694
zainzahid <zainzahid99@gmail.com>
2020-12-28 15:37:05 +0500

    Add project files.

---
+++ RecordAudioMvcApplication/Controllers/FormationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RecordAudioMvcApplication.Controllers
+{
+    public class FormationController : Controller
+    {
+        // GET: Formation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Save()
+        {
+            // Here Request.Files will recieve blob that we can upload to S3
+            return Content("Hello From Formation Controller");
+        }
+    }
+}
